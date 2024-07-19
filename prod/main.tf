6bd6b9226ed45b3b121f4080390101d8c95ac383
Manuel Ruiz <manu_124_2@hotmail.com>
2024-07-19 16:14:17 -0600

    Add tf files

---
+++ prod/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-utils" 
+    storage_account_name = "tfstateassessment" 
+    container_name       = "tfstate"                     
+    key                  = "assesmentprodstate.tfstate"       
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = "rg-assesment-prod"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  name = "devopsassessmentprod"
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+  }
+}
